d43ff6ade65212f59a81035994d8a0bd0375be89
meetqy <meetqy@icloud.com>
2024-06-07 20:10:07 +0800

    123

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,30 @@
-export default async function Home() {
+import { getTranslations } from "next-intl/server";
+import { Iconify } from "@/components/iconify";
+
+export default async function Home({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({ locale, namespace: "Index" });
+
   return (
     <main>
       <header className="h-16 border border-border bg-background/90">
-        <div className="container flex justify-between">
+        <div className="container flex h-full justify-between">
           {/* left */}
-          <div>left</div>
+          <div className="flex items-center text-xl font-bold uppercase">
+            <Iconify
+              fontSize={32}
+              className="mr-2"
+              icon="pepicons-pencil:swords"
+            />
+            xs.tool
+          </div>
           {/* center */}
-          <div className="flex-1"></div>
+          <div className="flex flex-1 items-center justify-center capitalize">
+            {t("title")}
+          </div>
           {/* right */}
           <div>right</div>
         </div>
---
+++ src/components/iconify.tsx
@@ -0,0 +1,5 @@
+import { Icon, type IconProps } from "@iconify/react";
+
+export const Iconify = (props: IconProps) => {
+  return <Icon {...props} />;
+};
