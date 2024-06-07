4b30766c4ba9c5ed11cb805bedeadff789b3d30e
meetqy <meetqy@icloud.com>
2024-06-07 17:21:55 +0800

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
-import "~/styles/globals.css";
+import "@/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
+import { Inter as FontSans } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/react";
+import { cn } from "@/lib/utils";
 
-import { TRPCReactProvider } from "~/trpc/react";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
   title: "Create T3 App",
@@ -16,7 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html
+      lang="en"
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable,
+      )}
+      suppressHydrationWarning
+    >
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
+import { CreatePost } from "@/app/_components/create-post";
+import { api } from "@/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
