5f555b2ac581f946c4c1e82923cc4bb30fec3f95
meetqy <meetqy@icloud.com>
2024-06-07 21:13:05 +0800

    123

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ const config = {
   rules: {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,32 +1,46 @@
 import { getTranslations } from "next-intl/server";
 import { Iconify } from "@/components/iconify";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home({
   params: { locale },
 }: {
   params: { locale: string };
 }) {
-  const t = await getTranslations({ locale, namespace: "Index" });
+  const t = await getTranslations({ locale, namespace: "Menu" });
+
+  // @ts-ignore
+  const menu = [0, 1, 2].map((item: number) => {
+    return (
+      <Button key={item} variant="ghost">
+        {/* @ts-ignore */}
+        <Link href="/">{t(`a${item}.name`)}</Link>
+      </Button>
+    );
+  });
 
   return (
     <main>
       <header className="h-16 border border-border bg-background/90">
         <div className="container flex h-full justify-between">
           {/* left */}
-          <div className="flex items-center text-xl font-bold uppercase">
+          <div className="flex w-48 items-center bg-primary text-xl font-bold uppercase">
             <Iconify
-              fontSize={32}
+              fontSize={36}
               className="mr-2"
               icon="pepicons-pencil:swords"
             />
             xs.tool
           </div>
+
           {/* center */}
-          <div className="flex flex-1 items-center justify-center capitalize">
-            {t("title")}
+          <div className="flex flex-1 items-center justify-center gap-4">
+            {menu}
           </div>
+
           {/* right */}
-          <div>right</div>
+          <div className="w-48 bg-primary">right</div>
         </div>
       </header>
     </main>
