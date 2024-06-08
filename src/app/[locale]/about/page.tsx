1ed87d0f22c1c7de46c0cc9c557fed6201ae923d
meetqy <meetqy@icloud.com>
2024-06-08 11:28:00 +0800

    123

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,3 +1,91 @@
-export default function Page() {
-  return <div>123</div>;
+import { Iconify } from "@/components/iconify";
+import { Badge } from "@/components/ui/badge";
+
+const features = [
+  {
+    name: "Invite team members",
+    description:
+      "Rerum repellat labore necessitatibus reprehenderit molestiae praesentium.",
+  },
+  {
+    name: "List view",
+    description:
+      "Corporis asperiores ea nulla temporibus asperiores non tempore assumenda aut.",
+  },
+  {
+    name: "Keyboard shortcuts",
+    description:
+      "In sit qui aliquid deleniti et. Ad nobis sunt omnis. Quo sapiente dicta laboriosam.",
+  },
+  {
+    name: "Calendars",
+    description:
+      "Sed rerum sunt dignissimos ullam. Iusto iure occaecati voluptate eligendi fugiat sequi.",
+  },
+  {
+    name: "Notifications",
+    description:
+      "Quos inventore harum enim nesciunt. Aut repellat rerum omnis adipisci.",
+  },
+  {
+    name: "Boards",
+    description:
+      "Quae sit sunt excepturi fugit veniam voluptatem ipsum commodi.",
+  },
+  {
+    name: "Reporting",
+    description:
+      "Eos laudantium repellat sed architecto earum unde incidunt. Illum sit dolores voluptatem.",
+  },
+  {
+    name: "Mobile app",
+    description:
+      "Nulla est saepe accusamus nostrum est est. Fugit voluptatum omnis quidem voluptatem.",
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Features
+            </p>
+            <p className="mt-6 flex flex-wrap gap-2 text-base leading-7 text-gray-600">
+              {[
+                "prisma",
+                "next.js",
+                "tailwindcss",
+                "iconify",
+                "trpc",
+                "next-intl",
+                "react-query",
+              ].map((item) => (
+                <Badge key={item} variant="outline" className="text-sm">
+                  {item}
+                </Badge>
+              ))}
+            </p>
+          </div>
+          <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-9">
+                <dt className="font-semibold text-gray-900">
+                  <Iconify
+                    icon={"lucide:check"}
+                    className="absolute left-0 top-1 h-5 w-5 text-indigo-500"
+                    aria-hidden="true"
+                  />
+                  {feature.name}
+                </dt>
+                <dd className="mt-2">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
 }
